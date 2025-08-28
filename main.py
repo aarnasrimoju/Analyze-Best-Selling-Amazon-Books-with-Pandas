f07f198f46ff0ac96e3e13e05d3de2691250a407
aarnasrimoju <SRIMOJUAARNA@GMAIL.COM>
2025-08-28 20:56:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+Name  ...        Genre
+0                      10-Day Green Smoothie Cleanse  ...  Non Fiction
+1                                  11/22/63: A Novel  ...      Fiction
+2            12 Rules for Life: An Antidote to Chaos  ...  Non Fiction
+3                             1984 (Signet Classics)  ...      Fiction
+4 n                              5,000 Awesome Facts  ...  Non Fiction
+
+[5 rows x 7 columns]
+(550, 7)
+Index(['Name', 'Author', 'User Rating', 'Reviews', 'Price', 'Year', 'Genre'], dtype='object')
+       User Rating       Reviews       Price         Year
+count   550.000000    550.000000  550.000000   2009.000000
+mean      4.618364  11953.281818   13.100000  2014.000000
+std       0.226980  11731.132017   10.842262     2013.165156
+min       3.300000     37.000000    0.000000  2009.000000
+25%       4.500000   4058.000000    7.000000  2011.000000
+50%       4.700000   8580.000000   11.000000  2014.000000
+75%       4.800000  17253.250000   16.000000  2017.000000
+max       4.900000  87841.000000  105.000000  2019.000000
